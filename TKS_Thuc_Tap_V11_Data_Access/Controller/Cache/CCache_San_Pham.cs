434a81d766d78af33561af2a97a5a9d982423960
LeKiet2711 <legiakiet03@gmail.com>
2024-04-12 14:00:49 +0700

    Add project files.

---
+++ TKS_Thuc_Tap_V11_Data_Access/Controller/Cache/CCache_San_Pham.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TKS_Thuc_Tap_V11_Data_Access.Controller.DM;
+using TKS_Thuc_Tap_V11_Data_Access.Entity.DM;
+using TKS_Thuc_Tap_V11_Data_Access.Utility;
+
+namespace TKS_Thuc_Tap_V11_Data_Access.Controller.Cache
+{
+    public class CCache_San_Pham
+    {
+        private static List<CDM_San_Pham> Arr_Data = new List<CDM_San_Pham>();
+        private static Dictionary<long, CDM_San_Pham> Dic_Data_ID = new Dictionary<long, CDM_San_Pham>();
+        private static Dictionary<string, CDM_San_Pham> Dic_Data_Code = new Dictionary<string, CDM_San_Pham>();
+
+        public static void Load_Cache_DM_San_Pham()
+        {
+            Arr_Data.Clear();
+            Dic_Data_ID.Clear();
+            Dic_Data_Code.Clear();
+
+            CDM_San_Pham_Controller v_objCtrData = new CDM_San_Pham_Controller();
+            List<CDM_San_Pham> v_arrTemp_Data = v_objCtrData.FQ_165_SP_sp_sel_List_For_Cache();
+
+            foreach (CDM_San_Pham v_objData in v_arrTemp_Data)
+                Add_Data(v_objData);
+        }
+
+        public static void Add_Data(CDM_San_Pham p_objData)
+        {
+            if (Dic_Data_ID.ContainsKey(p_objData.Auto_ID) == true || p_objData.Auto_ID == 0)
+                return;
+
+            Dic_Data_ID.Add(p_objData.Auto_ID, p_objData);
+            Arr_Data.Add(p_objData);
+
+            string v_strKey_Code = CUtility.Tao_Key(p_objData.Ma_San_Pham);
+            if (Dic_Data_Code.ContainsKey(v_strKey_Code) == false)
+                Dic_Data_Code.Add(v_strKey_Code, p_objData);
+        }
+
+        public static void Update_Data(CDM_San_Pham p_objData)
+        {
+            if (Dic_Data_ID.ContainsKey(p_objData.Auto_ID) == false || p_objData.Auto_ID == 0)
+                return;
+
+            Delete_Data(p_objData.Auto_ID);
+            Add_Data(p_objData);
+        }
+
+        public static void Delete_Data(long p_iAuto_ID)
+        {
+            if (Dic_Data_ID.ContainsKey(p_iAuto_ID) == false || p_iAuto_ID == 0)
+                return;
+
+            CDM_San_Pham v_objData = Dic_Data_ID[p_iAuto_ID];
+
+            Arr_Data.Remove(v_objData);
+            Dic_Data_ID.Remove(p_iAuto_ID);
+
+            string v_strKey_Code = CUtility.Tao_Key(v_objData.Ma_San_Pham);
+
+            Dic_Data_Code.Remove(v_strKey_Code);
+        }
+
+        public static CDM_San_Pham Get_Data_By_ID(long p_iID)
+        {
+            if (Dic_Data_ID.ContainsKey(p_iID) == true)
+                return Dic_Data_ID[p_iID];
+
+            return null;
+        }
+
+        public static CDM_San_Pham Get_Data_By_Code(string p_strCode)
+        {
+            string v_strKey = CUtility.Tao_Key(p_strCode);
+
+            if (Dic_Data_Code.ContainsKey(v_strKey) == true)
+                return Dic_Data_Code[v_strKey];
+
+            return null;
+        }
+
+    }
+}
---
+++ TKS_Thuc_Tap_V11_Data_Access/Entity/DM/CDM_San_Pham.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TKS_Thuc_Tap_V11_Data_Access.Controller.Cache;
+using TKS_Thuc_Tap_V11_Data_Access.Controller.DM;
+using TKS_Thuc_Tap_V11_Data_Access.Utility;
+
+namespace TKS_Thuc_Tap_V11_Data_Access.Entity.DM
+{
+    public class CDM_San_Pham
+    {
+        private long m_lngAuto_ID;
+        private long m_lngLoai_San_Pham_ID;
+        private long m_lngDon_Vi_Tinh_ID;
+        private string m_Ma_San_Pham;
+		private string m_Ten_San_Pham;
+		private string m_Ghi_Chu;
+		private int m_intdeleted;
+        private DateTime? m_dtmCreated;
+        private string m_strCreated_By;
+        private string m_strCreated_By_Function;
+        private DateTime? m_dtmLast_Updated;
+        private string m_strLast_Updated_By;
+        private string m_strLast_Updated_By_Function;
+
+        private string m_Ten_LSP;
+        private string m_Ten_Don_Vi_Tinh;
+
+
+		public CDM_San_Pham()
+        {
+            ResetData();
+        }
+
+        public void ResetData()
+        {
+            m_lngAuto_ID = CConst.INT_VALUE_NULL;
+            m_lngLoai_San_Pham_ID = CConst.INT_VALUE_NULL;
+            m_lngDon_Vi_Tinh_ID = CConst.INT_VALUE_NULL;
+            m_Ma_San_Pham = CConst.STR_VALUE_NULL;
+            m_Ten_San_Pham = CConst.STR_VALUE_NULL;
+            m_Ghi_Chu = CConst.STR_VALUE_NULL;
+            m_intdeleted = CConst.INT_VALUE_NULL;
+            m_dtmCreated = CConst.DTM_VALUE_NULL;
+            m_strCreated_By = CConst.STR_VALUE_NULL;
+            m_strCreated_By_Function = CConst.STR_VALUE_NULL;
+            m_dtmLast_Updated = CConst.DTM_VALUE_NULL;
+            m_strLast_Updated_By = CConst.STR_VALUE_NULL;
+            m_strLast_Updated_By_Function = CConst.STR_VALUE_NULL;
+        }
+
+        public long Auto_ID
+        {
+            get
+            {
+                return m_lngAuto_ID;
+            }
+            set
+            {
+                m_lngAuto_ID = value;
+            }
+        }
+
+        public long Loai_San_Pham_ID
+        {
+            get
+            {
+                return m_lngLoai_San_Pham_ID;
+            }
+            set
+            {
+                m_lngLoai_San_Pham_ID = value;
+            }
+        }
+
+		//public string Ten_Loai_San_Pham
+		//{
+		//	get
+		//	{
+  //              if (m_lngLoai_San_Pham_ID == 0)
+  //              {
+  //                  return "";   
+  //              }else return CCache_Loai_San_Pham.Get_Data_By_ID(m_lngLoai_San_Pham_ID).Ten_LSP;
+		//	}
+		//	set
+		//	{
+  //              if (m_lngLoai_San_Pham_ID == 0)
+  //              {
+		//			m_lngLoai_San_Pham_ID = 0;
+
+  //              }else CCache_Loai_San_Pham.Get_Data_By_ID(m_lngLoai_San_Pham_ID).Ten_LSP = value;
+		//	}
+		//}
+
+        public string Ten_LSP
+        {
+            get
+            {
+                return m_Ten_LSP;
+			}
+            set
+            {
+				m_Ten_LSP = value;
+			}
+        }
+
+		public string Ten_Don_Vi_Tinh
+		{
+			get
+			{
+				return m_Ten_Don_Vi_Tinh;
+			}
+			set
+			{
+				m_Ten_Don_Vi_Tinh = value;
+			}
+		}
+
+		public long Don_Vi_Tinh_ID
+        {
+            get
+            {
+                return m_lngDon_Vi_Tinh_ID;
+            }
+            set
+            {
+                m_lngDon_Vi_Tinh_ID = value;
+            }
+        }
+
+		//public string Ten_Don_Vi_Tinh
+		//{
+		//	get
+		//	{
+  //              if (m_lngDon_Vi_Tinh_ID == 0)
+  //              {
+  //                  return "";
+  //              }else return CCache_Don_Vi_Tinh.Get_Data_By_ID(m_lngDon_Vi_Tinh_ID).Ten_Don_Vi_Tinh;
+		//	}
+		//	set
+		//	{
+		//		if (m_lngDon_Vi_Tinh_ID == 0)
+		//		{
+		//			m_lngDon_Vi_Tinh_ID = 0;
+
+		//		}
+		//		else CCache_Don_Vi_Tinh.Get_Data_By_ID(m_lngDon_Vi_Tinh_ID).Ten_Don_Vi_Tinh = value;
+		//	}
+		//}
+
+		public string Ma_San_Pham
+		{
+			get
+			{
+				return m_Ma_San_Pham;
+			}
+            set
+			{
+				m_Ma_San_Pham = value;
+			}
+		}
+
+		public string Ten_San_Pham
+        {
+            get
+            {
+                return m_Ten_San_Pham;
+            }
+            set
+            {
+                m_Ten_San_Pham = value;
+            }
+        }
+
+        public string Ghi_Chu
+		{
+			get
+			{
+				return m_Ghi_Chu;
+			}
+            set
+			{
+				m_Ghi_Chu = value;
+			}
+		}
+
+        public int deleted
+        {
+            get
+            {
+                return m_intdeleted;
+            }
+            set
+            {
+                m_intdeleted = value;
+            }
+        }
+
+        public DateTime? Created
+        {
+            get
+            {
+                return m_dtmCreated;
+            }
+            set
+            {
+                m_dtmCreated = value;
+            }
+        }
+
+        public string Created_By
+        {
+            get
+            {
+                return m_strCreated_By;
+            }
+            set
+            {
+                m_strCreated_By = value.Trim();
+            }
+        }
+
+        public string Created_By_Function
+        {
+            get
+            {
+                return m_strCreated_By_Function;
+            }
+            set
+            {
+                m_strCreated_By_Function = value.Trim();
+            }
+        }
+
+        public DateTime? Last_Updated
+        {
+            get
+            {
+                return m_dtmLast_Updated;
+            }
+            set
+            {
+                m_dtmLast_Updated = value;
+            }
+        }
+
+        public string Last_Updated_By
+        {
+            get
+            {
+                return m_strLast_Updated_By;
+            }
+            set
+            {
+                m_strLast_Updated_By = value.Trim();
+            }
+        }
+
+        public string Last_Updated_By_Function
+        {
+            get
+            {
+                return m_strLast_Updated_By_Function;
+            }
+            set
+            {
+                m_strLast_Updated_By_Function = value.Trim();
+            }
+        }
+
+    }
+}
